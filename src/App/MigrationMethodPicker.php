ba186516e56482005b57777be514842965527b5a
Marko Dupor <marko.dupor@asseco-see.hr>
2021-08-09 13:12:07 +0200

    Added picker

---
+++ src/App/MigrationMethodPicker.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Asseco\BlueprintAudit\App;
+
+use Illuminate\Database\Schema\Blueprint;
+
+class MigrationMethodPicker
+{
+    public const SOFT = 'soft';
+    public const PARTIAL = 'partial';
+    public const FULL = 'full';
+
+    public static function pick(Blueprint $table, string $migrationConfig = null)
+    {
+        switch ($migrationConfig) {
+            case self::SOFT:
+                $table->timestamps();
+                $table->softDeletes();
+                break;
+            case self::PARTIAL:
+                $table->audit();
+                break;
+            case self::FULL:
+                $table->softDeleteAudit();
+                break;
+            default:
+                $table->timestamps();
+                break;
+        }
+    }
+}
